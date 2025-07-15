5c8035ff9b349c57f45baab9e84bae9c08dd5cde
hyperbora <hyperbora@gmail.com>
2025-07-15 21:57:37 +0900

    fix: 레이아웃 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 export default function Home() {
   return (
-    <div className="bg-red-500 p-6">
-      <h1 className="text-5xl font-bold text-center text-white">
-        Welcome to My Shopping Mall
-      </h1>
-      <p className="text-lg text-white text-center">
-        Explore our exciting products!
-      </p>
+    <div className="">
+      <h1 className="">Home Page</h1>
     </div>
   );
 }
