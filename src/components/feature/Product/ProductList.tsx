b304608b2b4afa8a5b4922a3f82770d35d2ea817
dchan <stpj0015@sorin.co.kr>
2025-07-16 17:20:00 +0900

    feat: 클라이언트 상태관리 제거

--- src/components/feature/Product/ProductList.tsx
+++ src/components/feature/Product/ProductList.tsx
@@ -1,16 +1,8 @@
-'use client'
-
-import { useEffect } from "react";
-import { useProductStore } from "@/store/useProductStore";
 import ProductCard from "./ProductCard";
+import products from "@/data.json"; 
 
 export default function ProductList() {
-  const { products, fetchProducts } = useProductStore();
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
+  
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       {products.map((p) => (
