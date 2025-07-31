83556c20d2d950bc4610f8992d1b48d269badbc8
hyperbora <hyperbora@gmail.com>
2025-07-31 16:17:52 +0900

    fix: 레이아웃 수정

--- src/components/feature/Product/ProductList.tsx
+++ src/components/feature/Product/ProductList.tsx
@@ -4,7 +4,7 @@ import ProductCard from "./ProductCard";
 export default function ProductList(prop: { products: Product[] }) {
   const { products } = prop;
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 px-4">
       {products.map((p) => (
         <ProductCard key={p.id} props={p} />
       ))}
