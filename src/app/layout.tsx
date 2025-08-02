3500928c210d8c627290fac5c1a2b8ac8b6469ea
hyperbora <hyperbora@gmail.com>
2025-08-02 15:10:18 +0900

    fix: 헤더 고정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-1 bg-gray-50">{children}</main>
+        <div className="fixed top-0 left-0 w-full z-50">
+          <Header />
+        </div>
+        <main className="flex-1 bg-gray-50 !mt-16">{children}</main>
         <Footer />
       </body>
     </html>
