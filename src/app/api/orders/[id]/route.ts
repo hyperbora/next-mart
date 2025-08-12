77ddc3ec04e9624a436d725475a7d27c2532c1fd
hyperbora <hyperbora@gmail.com>
2025-08-12 16:17:28 +0900

    fix: 주문 상세 api 파라미터 수정

--- src/app/api/orders/[id]/route.ts
+++ src/app/api/orders/[id]/route.ts
@@ -1,8 +1,11 @@
 import { supabase } from "@/lib/supabaseServer";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, context: { params: { id: string } }) {
-  const { id } = await context.params;
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
   const { data, error } = await supabase
     .from("orders")
     .select(
