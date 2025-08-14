ecb75ac5ef5d54adf9976af2a553f3c025c036c4
hyperbora <hyperbora@gmail.com>
2025-08-14 16:44:04 +0900

    fix: 로그인 예외 패턴 url 비교 연산자 수정

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -45,7 +45,7 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/auth") &&
     !request.nextUrl.pathname.startsWith("/error") &&
     !request.nextUrl.pathname.startsWith("/product") &&
-    !(request.nextUrl.pathname == "/")
+    !(request.nextUrl.pathname === "/")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
