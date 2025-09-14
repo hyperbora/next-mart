1100a80d481d62cb77d66400210319fd3e10979a
hyperbora <hyperbora@gmail.com>
2025-09-14 16:15:13 +0900

    feat: 배너 클릭 가능하도록 링크 추가

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -5,11 +5,12 @@ import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import Link from "next/link";
 
 const banners = [
-  "https://picsum.photos/seed/product1/600/300",
-  "https://picsum.photos/seed/product2/600/300",
-  "https://picsum.photos/seed/product3/600/300",
+  { src: "https://picsum.photos/seed/product1/600/300", link: "/product/1" },
+  { src: "https://picsum.photos/seed/product2/600/300", link: "/product/2" },
+  { src: "https://picsum.photos/seed/product3/600/300", link: "/product/3" },
 ];
 
 export default function MainBanner() {
@@ -23,13 +24,15 @@ export default function MainBanner() {
         pagination={{ clickable: true }}
         className="w-full h-full"
       >
-        {banners.map((src, idx) => (
+        {banners.map((banner, idx) => (
           <SwiperSlide key={idx} className="w-full h-full">
-            <img
-              src={src}
-              alt={`배너 ${idx + 1}`}
-              className="w-full h-full object-cover"
-            />
+            <Link href={banner.link}>
+              <img
+                src={banner.src}
+                alt={`배너 ${idx + 1}`}
+                className="w-full h-full object-cover"
+              />
+            </Link>
           </SwiperSlide>
         ))}
       </Swiper>
