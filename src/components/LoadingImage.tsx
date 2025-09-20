0f50b965553120e055ceeef72f189c79aa9d3ee5
hyperbora <hyperbora@gmail.com>
2025-09-20 13:03:58 +0900

    fix: 로딩이미지 컴포넌트 타임아웃 오류 수정

--- src/components/LoadingImage.tsx
+++ src/components/LoadingImage.tsx
@@ -1,23 +1,38 @@
 "use client";
 
 import Image, { ImageProps } from "next/image";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { ImageOff } from "lucide-react";
 
 export default function LoadingImage(props: ImageProps) {
   const [loading, setLoading] = useState(true);
   const [timeout, setTimeoutReached] = useState(false);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     if (!loading) return;
 
-    const timer = setTimeout(() => {
+    timerRef.current = setTimeout(() => {
       setTimeoutReached(true);
       setLoading(false);
     }, 10000);
 
-    return () => clearTimeout(timer);
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
   }, [loading]);
+
+  const handleLoad = () => {
+    setLoading(false);
+    setTimeoutReached(false);
+
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+      timerRef.current = null;
+    }
+  };
 
   if (timeout) {
     return (
@@ -30,18 +45,22 @@ export default function LoadingImage(props: ImageProps) {
   return (
     <>
       {loading && (
-        <div className="absolute flex items-center justify-center h-full">
-          <div className="w-6 h-6 border-2 border-gray-300 rounded-full border-t-green-500 animate-spin" />
+        <div className="flex items-center justify-center w-full h-full">
+          <div className=" w-6 h-6 border-2 border-gray-300 rounded-full border-t-green-500 animate-spin" />
         </div>
       )}
-      <Image
-        {...props}
-        alt={props.alt ?? ""}
-        className={`${props.className || ""} ${
-          loading ? "opacity-0" : "opacity-100"
-        }`}
-        onLoad={() => setLoading(false)}
-      />
+      {
+        <Image
+          {...props}
+          alt={props.alt ?? ""}
+          className={`${props.className || ""} ${
+            loading ? "opacity-0 absolute inset-0" : "opacity-100 relative"
+          }`}
+          onLoad={handleLoad}
+          width={props.width ?? 600}
+          height={props.height ?? 300}
+        />
+      }
     </>
   );
 }
