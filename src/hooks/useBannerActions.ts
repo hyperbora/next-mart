4e66c22d910a19f833309c06c4886d84896dd05f
hyperbora <hyperbora@gmail.com>
2025-09-21 10:21:27 +0900

    fix: api에서 에러 메시지 가져오는 방식 수정

--- src/hooks/useBannerActions.ts
+++ src/hooks/useBannerActions.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { getErrorMessage } from "@/utils";
+import { getApiErrorMesssage, getErrorMessage } from "@/utils";
 import { useState } from "react";
 import toast from "react-hot-toast";
 
@@ -23,7 +23,16 @@ export function useBannerActions(initialBanners: Banner[] = []) {
         body: JSON.stringify({ is_active: !currentStatus }),
       });
 
-      if (!res.ok) throw new Error("토글 실패");
+      if (!res.ok) {
+        const json = await res.json();
+
+        throw new Error(
+          getApiErrorMesssage(
+            json,
+            `배너 ${!currentStatus ? "활성화" : "비활성화"}에 실패했습니다.`
+          )
+        );
+      }
 
       setBanners((prev) =>
         prev.map((b) => (b.id === id ? { ...b, is_active: !currentStatus } : b))
@@ -39,14 +48,15 @@ export function useBannerActions(initialBanners: Banner[] = []) {
 
   // ✅ 배너 삭제 함수
   const handleDelete = async (id: number) => {
-    if (!confirm("정말 삭제하시겠습니까?")) return;
-
     try {
       const res = await fetch(`/api/admin/banners/${id}`, {
         method: "DELETE",
       });
 
-      if (!res.ok) throw new Error("삭제 실패");
+      if (!res.ok) {
+        const json = await res.json();
+        throw new Error(getApiErrorMesssage(json, "배너 삭제가 실패했습니다."));
+      }
 
       setBanners((prev) => prev.filter((b) => b.id !== id));
       toast.success("배너가 삭제되었습니다.");
--- src/utils/error.ts
+++ src/utils/error.ts
@@ -22,3 +22,22 @@ export function getErrorMessage(error: unknown): string {
     return "알 수 없는 오류가 발생했습니다.";
   }
 }
+
+export function getApiErrorMesssage(
+  json: unknown,
+  defaultMessage: string
+): string {
+  if (json instanceof Error) {
+    return json.message;
+  }
+
+  if (
+    typeof json === "object" &&
+    json !== null &&
+    "error" in json &&
+    typeof (json as { error: unknown }).error === "string"
+  ) {
+    return (json as { error: string }).error;
+  }
+  return defaultMessage;
+}
