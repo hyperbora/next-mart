d5e18c2041072f9cd2f00f5e55613aa1ff2dd0d5
hyperbora <hyperbora@gmail.com>
2025-09-29 14:03:54 +0900

    fix: 검색결과 상품 콤포넌트 디자인 수정

--- src/components/SearchResultProduct.tsx
+++ src/components/SearchResultProduct.tsx
@@ -1,22 +1,28 @@
 import { type Product } from "@/types";
 import LoadingImage from "./LoadingImage";
+import Link from "next/link";
 
 export default function SearchResultProduct(product: Product) {
   return (
-    <div key={product.id} className="p-2 border rounded">
-      {product.image_url && (
-        <LoadingImage
-          src={product.image_url}
-          alt={product.title}
-          className="object-cover w-full h-32"
-          width={300}
-          height={300}
-        />
-      )}
-      <p className="mt-2 text-sm line-clamp-2">{product.title}</p>
-      <p className="font-bold text-green-600">
-        ₩{product.price.toLocaleString()}
-      </p>
-    </div>
+    <Link
+      href={`/product/${product.id}`}
+      className="block p-2 border rounded-md cursor-pointer hover:shadow-lg transition-transform duration-300 ease-in-out hover:scale-105"
+    >
+      <div>
+        {product.image_url && (
+          <LoadingImage
+            src={product.image_url}
+            alt={product.title}
+            className="object-cover w-full h-32"
+            width={300}
+            height={300}
+          />
+        )}
+        <p className="mt-2 text-sm line-clamp-2">{product.title}</p>
+        <p className="font-bold text-green-600">
+          ₩{product.price.toLocaleString()}
+        </p>
+      </div>
+    </Link>
   );
 }
